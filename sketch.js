2599cf03d27964485d1103b125074fba95647bc9
Adwit Singh <67267753+TERRYTGPM@users.noreply.github.com>
2020-11-26 20:16:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var hr, mn, sc;
+var end, end2, end3
+
+
+function setup() {
+  createCanvas(450,450);
+  angleMode(DEGREES);
+}
+
+function draw() {
+
+  background(0);
+
+  translate(200,200)
+  rotate(-90);  
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+  
+  strokeWeight(8);
+  stroke("Cyan");
+  noFill();
+  end = map(sc ,0 ,60 ,0 ,360);
+  arc(0, 0, 300, 300 , 0, end);
+  
+  stroke("Green");
+  end2 = map(mn, 0, 60, 0, 360);
+  arc(0, 0, 280, 280, 0, end2);
+  
+  stroke("Blue");
+  end3 = map(hr % 12 ,0 ,12 ,0 , 360);
+  arc(0 ,0 , 260, 260, 0, end3);
+  
+  push();
+  rotate(end);
+  stroke("blue");
+  line(0,0,100,0);
+  pop();
+  
+  
+  push();
+  rotate(end2);
+  stroke("red");
+  line(0,0,75,0);
+  pop();
+  
+  push();
+  rotate(end3);
+  stroke("green");
+  line(0,0,50,0);
+  pop();
+
+}  
